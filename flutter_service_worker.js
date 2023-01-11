f46e887f1b209924aa2eb7795efd71d56fa445b0
nikdit <110746073+nikdit@users.noreply.github.com>
2023-01-11 20:46:49 +0100

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -115,9 +115,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "2b3f4aad5c6128aa420c28c158e3e2d3",
-"/": "2b3f4aad5c6128aa420c28c158e3e2d3",
-"main.dart.js": "95414a4cf4d9807da92ace34fe34898b",
+"index.html": "d998038c2e0a825c49728c9b1056c839",
+"/": "d998038c2e0a825c49728c9b1056c839",
+"main.dart.js": "400041f06f5f743b1dc8a6578976ca81",
 "manifest.json": "cdfe77bdfa9eb00121bdb1970ebb32c2",
 "README.md": "b69e50eb3d02aa532df0c9eeb5c10095",
 "version.json": "bf43eb28f1cea5cb072fb1d0c23c0f4b"
